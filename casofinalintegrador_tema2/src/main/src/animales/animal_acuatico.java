fbb7b54492c11f053b55fc1f48720c85d203e7f2
loreeenacano <160004312+loreeenacano@users.noreply.github.com>
2024-03-05 09:54:23 +0000

    cambios

---
+++ casofinalintegrador_tema2/src/main/src/animales/animal_acuatico.java
@@ -0,0 +1,53 @@
+package animales;
+
+public class animal_acuatico extends animal {
+    boolean aletas;
+    boolean branquias;
+    boolean escamas;
+    String tipo_agua;
+
+    public animal_acuatico(String nombre, String especie, String raza, String color, String sexo, String edad,
+    String peso, String altura, String tipo, String habitat, String alimentacion, String reproduccion,
+    String cuidados, String enfermedades, String vacunas, int id,boolean salud, boolean comportamiento, boolean aletas, boolean branquias,
+    boolean escamas, String tipo_agua) {
+    super(nombre, especie, raza, color, sexo, edad, peso, altura, tipo, habitat, alimentacion, reproduccion, cuidados,
+        enfermedades, vacunas, id, salud, comportamiento); //super invoca al constructor de animal
+    this.aletas = aletas;
+    this.branquias = branquias;
+    this.escamas = escamas;
+    this.tipo_agua = tipo_agua;
+}
+// is se usa para booleans para ver si tiene o no, (si no es boolean uso el get)
+public boolean isAletas() {
+    return aletas;
+}
+
+public void setAletas(boolean aletas) {
+    this.aletas = aletas;
+}
+
+public boolean isBranquias() {
+    return branquias;
+}
+
+public void setBranquias(boolean branquias) {
+    this.branquias = branquias;
+}
+
+public boolean isEscamas() {
+    return escamas;
+}
+
+public void setEscamas(boolean escamas) {
+    this.escamas = escamas;
+}
+
+public String getTipo_agua() {
+    return tipo_agua;
+}
+
+public void setTipo_agua(String tipo_agua) {
+    this.tipo_agua = tipo_agua;
+}
+
+}

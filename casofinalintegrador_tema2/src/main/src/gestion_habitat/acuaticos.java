11884e497350c8ad85529ab7bd6282c0fb82823f
loreeenacano <160004312+loreeenacano@users.noreply.github.com>
2024-03-06 08:31:12 +0000

    cambios

--- casofinalintegrador_tema2/src/main/src/gestion_habitat/acuaticos.java
+++ casofinalintegrador_tema2/src/main/src/gestion_habitat/acuaticos.java
@@ -1,8 +1,52 @@
 package archetype-resources.src.gestion_habitat;
 
 public class acuaticos extends habitat {
-    public acuaticos(){
+    boolean puede_nadar;
 
+    public acuaticos(float temperatura,float humedad,boolean limpieza,boolean puede_nadar) {
+        super(temperatura, humedad, limpieza);
+        this.puede_nadar = puede_nadar;
     }
-    public String toString 
+
+
+    @Override //se coloca delante de un metodo heredado para modificar su comportamiento original
+        public String toString() {
+        return "acuaticos []";
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        // TODO Auto-generated method stub
+        super.finalize();
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+
+
+    public boolean isPuede_nadar() {
+        return puede_nadar;
+    }
+
+
+    public void setPuede_nadar(boolean puede_nadar) {
+        this.puede_nadar = puede_nadar;
+    }    
 }
+    
+
